65f011517145ae5da676e0d43066445c649dba2e
Xiang Zhu <zhuxiang1212@hotmail.com>
2022-02-01 18:09:05 +0800

    [Feature]: PBKDF2 Demo

---
+++ PBKDF2/PBKDF2Demo/PBKDF2Service.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace PBKDF2Demo;
+
+public class PBKDF2Service
+{
+    public static void Test()
+    {
+        var password = "P@ssw0rd"; // Password
+        var salt = Encoding.ASCII.GetBytes("this_is_salt"); // Salt
+        
+        var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 1500, HashAlgorithmName.SHA256); // PBKDF2
+        Console.WriteLine($"PBKDF2: {BitConverter.ToString(pbkdf2.GetBytes(15))}");
+        Console.WriteLine($"HashAlgorithm: {pbkdf2.HashAlgorithm}");
+        Console.WriteLine($"IterationCount: {pbkdf2.IterationCount}");
+    }
+}
---
+++ PBKDF2/PBKDF2Demo/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PBKDF2Demo;
+
+PBKDF2Service.Test();
+Console.ReadLine();
